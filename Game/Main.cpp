a87a22086bf726b1d63dcdee7f6ee4cdfaba9d23
mmargolin <mmargolin@student.neumont.edu>
2021-08-02 16:31:22 -0600

    SDL Start

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <SDL.h>
 
-int main()
+int main(int, char**)
 {
-	std::cout << ":)\n";
+
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+
+		std::cout << "SDL_Init Error." << SDL_GetError() << std::endl;
+		return 1;
+
+	}
+
+	SDL_Window* window = SDL_CreateWindow("Gat150 | :)", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+	
+	/*
+	SDL_Window* window2 = SDL_CreateWindow("Gat150 | :)", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+	SDL_Window* window3 = SDL_CreateWindow("Gat150 | :)", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+	SDL_Window* window4 = SDL_CreateWindow("Gat150 | :)", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+	SDL_Window* window5 = SDL_CreateWindow("Gat150 | :)", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+	SDL_Window* window6 = SDL_CreateWindow("Gat150 | :)", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+	SDL_Window* window7 = SDL_CreateWindow("Gat150 | :)", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+	SDL_Window* window8 = SDL_CreateWindow("Gat150 | :)", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+	*/
+
+	if (window == nullptr)
+	{
+		std::cout << "SDL_CreateWindowError: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	
+	}
+
+	// Wait for keypress to exit
+	std::getchar();
+
+
+	SDL_Quit();
 
 }
