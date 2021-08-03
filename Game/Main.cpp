43d0ee78da97503eb076724fbf53c74efc744234
mmargolin <mmargolin@student.neumont.edu>
2021-08-02 22:41:26 -0600

    Assignment

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -12,17 +12,37 @@ int main(int, char**)
 
 	}
 
-	SDL_Window* window = SDL_CreateWindow("Gat150 | :)", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
 	
+	SDL_Window* window = SDL_CreateWindow("Gat150 | :)", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+
+	//USE THIS AT OWN DISCRETION, IS FUN BUT BE CAREFUL
+
 	/*
-	SDL_Window* window2 = SDL_CreateWindow("Gat150 | :)", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
-	SDL_Window* window3 = SDL_CreateWindow("Gat150 | :)", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
-	SDL_Window* window4 = SDL_CreateWindow("Gat150 | :)", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
-	SDL_Window* window5 = SDL_CreateWindow("Gat150 | :)", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
-	SDL_Window* window6 = SDL_CreateWindow("Gat150 | :)", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
-	SDL_Window* window7 = SDL_CreateWindow("Gat150 | :)", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
-	SDL_Window* window8 = SDL_CreateWindow("Gat150 | :)", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+	SDL_Window* windows[450];
+
+	for (int i = 0; i < 50; i++)
+	{
+		
+			windows[i] = SDL_CreateWindow("Gat150 | :)", 100 + (i * 10), 100 + (i * 10), 800, 600, SDL_WINDOW_SHOWN);
+		
+			windows[i + 25] = SDL_CreateWindow("Gat150 | :)", 200 + (i * 10), 100 + (i * 10), 800, 600, SDL_WINDOW_SHOWN);
+			windows[i + 50] = SDL_CreateWindow("Gat150 | :)", 300 + (i * 10), 100 + (i * 10), 800, 600, SDL_WINDOW_SHOWN);
+			windows[i + 75] = SDL_CreateWindow("Gat150 | :)", 400 + (i * 10), 100 + (i * 10), 800, 600, SDL_WINDOW_SHOWN);
+			windows[i + 100] = SDL_CreateWindow("Gat150 | :)", 500 + (i * 10), 100 + (i * 10), 800, 600, SDL_WINDOW_SHOWN);
+		
+	}
+
+	if (windows == nullptr)
+	{
+		std::cout << "SDL_CreateWindowError: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+
+	}
 	*/
+
+	
+
 
 	if (window == nullptr)
 	{
