0cd64088ce3525dd481e7909712d74b4ba1b9bc6
mmargolin <mmargolin@student.neumont.edu>
2021-08-31 15:45:34 -0600

    asdf

---
+++ Engine/Object/Tilemap.cpp
@@ -0,0 +1,16 @@
+#include "Tilemap.h"
+
+namespace smile
+{
+	void Tilemap::Create()
+	{
+	}
+	bool Tilemap::Write(const rapidjson::Value& value) const
+	{
+		return false;
+	}
+	bool Tilemap::Read(const rapidjson::Value& value)
+	{
+		return false;
+	}
+}
---
+++ Engine/Object/Tilemap.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Object.h"
+#include "Core/Serializable.h"
+#include <vector>
+
+namespace smile
+{
+	class Tilemap : public Object, public ISerializable
+	{
+	public:
+		void Create() override;
+
+		virtual bool Write(const rapidjson::Value& value) const override;
+		virtual bool Read(const rapidjson::Value& value) override;
+
+	public:
+		class Scene* scene = nullptr;
+
+		int numTilesX{ 0 };
+		int numTilesY{ 0 };
+
+		Vector2 size;	
+		std::vector<std::string> tileNames; 
+
+		std::vector<int> tiles;
+	};
+
+}
